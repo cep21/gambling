56d1ef6f74f4d67d800002e1866dc15de5a9a5a0
Jack Lindamood <jack@signalfuse.com>
2014-11-18 18:58:38 -0800

    More hand hasher

--- src/hand_hasher.rs
+++ src/hand_hasher.rs
@@ -8,22 +8,50 @@ pub struct DealerHandHasher;
 
 impl HandHasher for DealerHandHasher {
     fn hash_hand(rules: &BJRules, hand: &BJHand) -> Vec<u8> {
-        // The dealer's hand is only important on 17 if the dealer hits S17
         let mut score = hand.score();
         // All scores > 22 are the same to us
         if score > 22 {
             score = 22;
         }
-        // Treat soft 18 same as hard 18 if the dealer stands on both
-        let is_soft = match hand.is_soft() {
-            false => false,
-            true => match rules.dealer_hits_soft_score(hand.score()) {
-                false => false,
-                true => true,
+        let is_soft = hand.is_soft() &&
+        // Treat soft 17 same as hard 17 if the dealer stands on both
+            rules.dealer_hits_soft_score(hand.score());
+
+        // Hash together the score and softness
+        assert!(score <= 22);
+        let mut hash: u8 = score as u8;
+        if is_soft {
+            hash += 23;
+        }
+        vec![hash]
+    }
+}
+
+pub struct PlayerHandHasher;
+
+impl HandHasher for PlayerHandHasher {
+    fn hash_hand(rules: &BJRules, hand: &BJHand) -> Vec<u8> {
+        assert!(hand.len() >= 1);
+        let mut score = hand.score();
+        // All scores > 22 are the same to us
+        if score > 22 {
+            score = 22;
+        }
+        let is_soft = hand.is_soft();
+
+        // Number of cards in the hand only matter if it's 1, 2, or 3+
+        let cards_in_hand_hash = {
+            if hand.len() == 1 {
+                0u
+            } else if hand.len() == 2 {
+                1u
+            } else {
+                2u
             }
         };
+
         // Hash together the score and softness
-        assert!(score <= 21);
+        assert!(score <= 22);
         let mut hash: u8 = score as u8;
         if is_soft {
             hash += 22;
